bcb2ba07dac9c47a840f0f5259675c04c30b701e
MarkoLauer <marko.lauer@tktk.ee>
2023-05-14 21:02:32 +0300

    Week15 OOP and Files

---
+++ Week15FrozenSecretSanta/Program.cs
@@ -0,0 +1,53 @@
+﻿
+//string[] data = GetDataFromMyFile();
+
+List<Gift> myGifts = new List<Gift>();
+string[] giftsFromFile = GetDataFromMyFile();
+foreach (string line in giftsFromFile)
+{
+    string[] tempArray = line.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+    Gift newGift = new Gift(tempArray[0],tempArray[1]);
+    myGifts.Add(newGift);
+}
+foreach (Gift giftFromList in myGifts)
+{
+    Console.WriteLine($" {giftFromList.Name} wants {giftFromList.Present} for Christmas.");
+}
+
+
+static string[] GetDataFromMyFile()
+{
+    string filepath = @"C:\Users\Marko\frozen.txt";
+    //string[] dataFromFile = File.ReadAllLines(filepath);
+
+    return File.ReadAllLines(filepath);
+}
+
+
+static void ReadDataFromArray(string[] someArray)
+{
+    foreach (string line in someArray)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+class Gift
+{
+    string name;
+    string present;
+
+    public Gift(string _name, string _present)
+    {
+        name = _name;
+        present = _present;
+    }
+    public string Name
+    {
+        get { return name; }
+    }
+    public string Present
+    {
+        get { return present; }
+    }
+}
---
+++ Week15OOPandFiles/Program.cs
@@ -0,0 +1,66 @@
+﻿
+List<Movie> myMovies = new List<Movie>();
+string[] data = GetDataFromMyFile();
+//ReadDataFromArray(data);
+
+foreach(string line in data)
+{
+    string[] tempArray = line.Split(new char[] {';'},StringSplitOptions.RemoveEmptyEntries);
+    //Console.WriteLine("Temp array");
+    //ReadDataFromArray(tempArray);
+    Movie newMovie = new Movie(tempArray[0], tempArray[2], tempArray[1]);
+    myMovies.Add(newMovie);
+}
+
+foreach (Movie movie in myMovies)
+{
+    Console.WriteLine($"One of my favourite movies {movie.Title} was released on {movie.Year}. Rating: {movie.Rating}");
+}
+
+
+
+static string[] GetDataFromMyFile()
+{
+    string filepath = @"C:\Users\Marko\movies.txt";
+    //string[] dataFromFile = File.ReadAllLines(filepath);
+
+    return File.ReadAllLines(filepath);
+}
+
+static void ReadDataFromArray(string[] someArray)
+{
+    foreach (string line in someArray) 
+    {
+        Console.WriteLine(line);
+    }
+}
+
+class Movie
+{
+    string title; //fields
+    string year;
+    string rating;
+
+    public string Title
+    {
+        get { return title; }
+    }
+
+    public string Year
+    { 
+        get { return year; }
+    }
+
+    public string Rating
+    {
+        get { return rating; }
+    }
+
+    public Movie(string _title, string _year, string _rating)
+    {
+        title = _title;
+        year = _year;
+        rating = _rating;
+    }
+
+}
